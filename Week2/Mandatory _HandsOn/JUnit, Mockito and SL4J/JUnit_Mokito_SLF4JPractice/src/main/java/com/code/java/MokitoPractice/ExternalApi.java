fe5d7e5caec47bf30a9f8afc92fb114f41de8dcd
Nayana Ghosh <nayanaghosh20208193@gmail.com>
2025-08-10 22:39:08 +0530

    Delete Week2 directory

--- Week2/Mandatory _HandsOn/JUnit, Mockito and SL4J/JUnit_Mokito_SLF4JPractice/src/main/java/com/code/java/MokitoPractice/ExternalApi.java
+++
@@ -1,9 +0,0 @@
-package com.code.java.MokitoPractice;
-
-public class ExternalApi {
-    public String getData() {
-        // In real life, this could make an API call
-        return "Welcome to My Service";
-    }
-}
-
